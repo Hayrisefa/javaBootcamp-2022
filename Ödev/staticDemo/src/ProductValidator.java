2d22a2b2b40db7379779883066b01fb975514c80
Hayrisefa <56302939+Hayrisefa@users.noreply.github.com>
2022-10-09 16:28:11 +0300

    33-45

---
+++ Ödev/staticDemo/src/ProductValidator.java
@@ -0,0 +1,25 @@
+public class ProductValidator {
+    static {
+        System.out.println("Static Yapıcı blok çalıştı");
+    }
+    public ProductValidator(){
+        System.out.println("Yapıcı blok çalıştı");
+    }
+    public static boolean isValid(Product product) {// Staticler direk ProducValidator. diye çagırılabiliyor
+        if (product.price > 0 && !product.name.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public void bisey(){
+
+    }
+    public static class BaskaBirClass{
+       public static void  sil(){
+
+       }
+    }
+
+}
+
